d5ffb7988d39195533b507ced90f091be293eaaf
GcellyGomes <gicelly.gomes@etec.sp.gov.br>
2023-09-15 20:05:04 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int numeroDigitado, centenas, dezenas, unidades;
+
+Console.Write("Digite um número...");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"O número digitado e {numeroDigitado}");
+
+centenas = numeroDigitado / 100;
+dezenas = (numeroDigitado % 100) / 10;
+unidades = (numeroDigitado % 100) % 10;
+
+Console.WriteLine($"{unidades} unidades(s)");
+Console.WriteLine($"{dezenas} dezenas(s)");
+Console.WriteLine($"{centenas} centenas(s)");
